8024b9a2b983501a71f478fbfa13b0c2cd7b3bcb
Amy Larson <larsonam@augsburg.edu>
2023-01-24 17:10:56 -0600

    fixed tests

--- Exercises/UnitTestCircles/UnitTestCircle.java
+++ Exercises/UnitTestCircles/UnitTestCircle.java
@@ -1,4 +1,8 @@
 public class UnitTestCircle extends UnitTest {
+
+    int MyVar = 0;
+    int This = 5;
+
     public static void main(String[] args) {
         testOverlaps();
     }
@@ -9,11 +13,11 @@ public class UnitTestCircle extends UnitTest {
     public static void testOverlaps() {
         System.out.println("_________________ TESTING Overlaps()");
 
-        Circle circle = new Circle(new Point(10,10), 1);
-        Circle other = new Circle(new Point(20,10), 1);
+        Circle circle = new Circle(new Point(10, 10), 1);
+        Circle other = new Circle(new Point(20, 10), 1);
 
         // Testing no overlap between circles
-        test(false,circle.overlaps(other),"Non-overlapping circles.");
+        test(false, circle.overlaps(other), "Non-overlapping circles.");
 
     }
 }
